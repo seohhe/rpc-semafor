d06b6e89e3d3ce95358f7b808795f1554a664555
Marta Sitkowska <marta.sitkowska@akai.org.pl>
2018-09-27 10:27:34 +0200

    final

--- clients.js
+++ clients.js
@@ -1,13 +1,73 @@
 console.log("client")
 const jayson = require('jayson')
 
-// create a client
-const client = jayson.client.http({
-  port: 3000
-});
+class Client {
+  constructor() {
+    this.connection = jayson.client.http({
+      port: 3001
+    });
+  }
+  request(...args) {
+    return this.connection.request(...args);
+  }
+  increase(v = 1, callback = () => {}) {
+    this.request('increase', [v, this.name], callback)
+  }
+  decrease(v = 1, callback = () => {}) {
+    this.request('decrease', [v, this.name], callback)
+  }
+}
 
-// invoke "add"
-client.request('add', [1, 1], (err, response) => {
-  if(err) throw err
-  console.log(response.result)
-})
+class Reader extends Client {
+  constructor(name) {
+    super();
+    this.name = name;
+  }
+  enterLibrary(time) {
+    console.log(`${this.name} chce wejść do biblioteki`)
+    this.decrease(1, (err, response) => {
+      console.log(`${this.name} wchodzi do biblioteki`)
+      if(err) throw err;
+      setTimeout(_ => {
+        console.log(`${this.name} chce wyjść z biblioteki`)
+        this.increase(1, (err, response) => {
+          if(err) throw err;
+          console.log(`${this.name} wychodzi z biblioteki`)
+        })
+      }, time)
+    })
+  }
+}
+
+class Writer extends Client {
+  constructor(name) {
+    super();
+    this.name = name;
+  }
+  enterLibrary(time) {
+    console.log(`Pisarz ${this.name} chce wejść do biblioteki`)
+    this.decrease(10, (err, response) => {
+      console.log(`Pisarz ${this.name} wchodzi do biblioteki`)
+      if(err) throw err;
+      setTimeout(_ => {
+        console.log(`Pisarz ${this.name} chce wyjść z biblioteki`)
+        this.increase(10, (err, response) => {
+          if(err) throw err;
+          console.log(`Pisarz ${this.name} wychodzi z biblioteki`)
+        })
+      }, time)
+    })
+  }
+}
+
+const adam = new Reader('Adam');
+const stefan = new Writer('Stefan');
+const basia = new Reader('Basia');
+
+for(let i = 0; i<10; i++) {
+  setTimeout(() => {
+    adam.enterLibrary(1000);
+    stefan.enterLibrary(3000);
+    basia.enterLibrary(2000);
+  }, 1000 * i * 6)
+}
--- semafor.js
+++ semafor.js
@@ -1,17 +1,49 @@
 console.log("semafor")
 const jayson = require('jayson')
 
-// create a server
-const server = jayson.server({
-  add: (args, callback) => {
-    callback(null, args[0] - args[1])
-  },
-  increase: () => {
+class Semafor {
+  constructor() {
+    this.val = 10
+    this._queueMore = []
 
-  },
-  decrease: () => {
+    this.server = jayson.server({
+      increase: (args, callback) => this.increase(args[0], callback),
+      decrease: (args, callback) => this.decrease(args[0], callback)
+    })
+    this.server.http().listen(3001)
 
-  },
-})
+    console.log(`Semafor ma wartość: ${this.val}`)
+  }
+  increase(value, callback) {
+    this.val += value
+    callback(null, this.val)
 
-server.http().listen(3000)
+    if(this._queueMore.length > 0) {
+      const cb = this._queueMore.shift()
+      if(cb()) {
+      }
+    }
+  }
+  decrease(value, callback, tryagain = false) {
+    if(this.val - value >= 0) {
+      this.val -= value
+      this.log(`Zmniejszono wartość o: ${value}. Aktualna wartość to: ${this.val}`);
+      callback(null, this.val)
+      return true
+    } else {
+        this._queueMore.push(() => {
+          this.decrease(value, callback, true)
+        })
+    }
+    return false
+  }
+  get val() {
+    return this._semval
+  }
+  set val(x) {
+    console.log(`semval = ${x}`)
+    this._semval = x
+  }
+}
+
+new Semafor();
